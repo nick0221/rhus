d6062da81ad68eb10f8e2c2ad90fb364c9c3807c
nick0221 <nick022190@gmail.com>
2023-11-02 16:06:25 +0800

    Family History added

---
+++ app/Models/FamilyHistory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FamilyHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['description', 'historyDate', 'individual_id' ];
+
+}
